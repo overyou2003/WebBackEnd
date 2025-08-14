c39886c98da61cb68408c7abc2ac48c65ed8bf82
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-14 22:51:51 +0700

    feat(model): add new field to Model

--- src/models/user.js
+++ src/models/user.js
@@ -12,6 +12,14 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         unique: true,
       },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
